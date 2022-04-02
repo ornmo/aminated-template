c842a7c90d003a9a6cde36302e325dfb2cd808e2
ornmo <ornaonline@gmail.com>
2022-04-02 15:51:23 +0100

    Completed and fully functioning

---
+++ script.js
@@ -0,0 +1,6 @@
+function toggleNavbar(collapseNavbar) {
+    document.getElementById(collapseNavbar).classList.toggle("hidden");
+    document.getElementById(collapseNavbar).classList.toggle("block");
+}
+
+// alert('stop')
